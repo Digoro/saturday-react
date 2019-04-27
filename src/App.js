4e5912e7de83ab842fd6d92b476eb69019529560
shlee <lsh00124@naver.com>
2019-04-27 09:09:11 +0900

    04

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import LifecycleSample from './LifecycleSample';
+import IterationSample from './IterationSample';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getRandomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <IterationSample></IterationSample>
+        <button onClick={this.handleClick}>랜덤색상</button>
+        <LifecycleSample color={this.state.color}></LifecycleSample>
+      </div>
+    )
+  }
 }
 
 export default App;
+
